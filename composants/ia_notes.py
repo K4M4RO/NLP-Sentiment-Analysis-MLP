8d38c92159f586c78288ea22f4e9e42a9b24786f
fkmh-ops <fketmh@gmail.com>
2026-02-17 16:23:43 +0100

    Ajustements

--- composants/ia_notes.py
+++ composants/ia_notes.py
@@ -8,9 +8,17 @@ import pandas as pd
 import numpy as np
 import joblib, os
 
+
+class C: # Classe qui permet d'appliquer de la couleur aux print (pas important)
+    VERT = '\033[92m'
+    ROUGE = '\033[91m'
+    JAUNE = '\033[93m'
+    BLEU = '\033[94m'
+    RESET = '\033[0m' # Important pour arrêter la couleur
+
 class NotesPredicteur:
     _x: list[str]
-    _y: list[str] | list[int]
+    _y: list[str]
     _embeddings: list[list[float]]
     _mlp: object
     _scaler:object
@@ -34,7 +42,7 @@ class NotesPredicteur:
             learning_rate_init=0.001       # Taux d'apprentissage initial
         )
 
-    def charger_data(self,chemin_dataset:str) -> None:
+    def charger_data(self,chemin_dataset:str="data/books_data_test.csv") -> None:
         df = pd.read_csv(chemin_dataset, sep=",", on_bad_lines='skip')
         self._y = df["review/score"].to_numpy()
         self._y = np.array(self._y).reshape(-1, 1)
@@ -47,15 +55,15 @@ class NotesPredicteur:
             os.makedirs(self._dossier_sauvegarde)
         chemin = os.path.join(self._dossier_sauvegarde, nom_fichier)
         np.save(chemin,self._embeddings)
-        print(f"Embeddings sauvegardé dans {nom_fichier}!")
+        print(f"{C.VERT}Embeddings sauvegardé dans {nom_fichier}!{C.RESET}")
     
     def charger_embeddings(self, nom_fichier="embeddings.npy"):
         chemin = os.path.join(self._dossier_sauvegarde, nom_fichier)
         try:
             self._embeddings = np.load(chemin)
-            print(f"Embeddings chargé depuis {nom_fichier}!")
+            print(f"{C.VERT}Embeddings chargé depuis {nom_fichier}!{C.RESET}")
         except:
-            print("Erreur lors du chargement du embeddings")
+            print(f"{C.ROUGE}Erreur lors du chargement du embeddings !{C.RESET}")
         
     def sauvegarder_cerveau(self, nom_fichier="ia_notes_sauvegarde.joblib"):
         chemin = os.path.join(self._dossier_sauvegarde, nom_fichier)
@@ -64,7 +72,7 @@ class NotesPredicteur:
             "scaler": self._scaler
         }
         joblib.dump(package,chemin)
-        print(f"IA sauvegardée dans {nom_fichier} !")
+        print(f"{C.VERT}IA sauvegardée dans {nom_fichier} !{C.RESET}")
     
     def charger_cerveau(self,nom_fichier="ia_notes_sauvegarde.joblib"):
         chemin = os.path.join(self._dossier_sauvegarde,nom_fichier)
@@ -72,9 +80,9 @@ class NotesPredicteur:
             package = joblib.load(chemin)
             self._mlp = package["mlp"]
             self._scaler = package["scaler"]
-            print("Cerveau chargé !")
+            print(f"{C.VERT}Cerveau chargé !{C.RESET}")
         except:
-            print("Erreur lors du chargement. Vérifiez que le fichier .joblib existe et qu'il se trouve à la racine")
+            print(f"{C.ROUGE}Erreur lors du chargement. Vérifiez que le fichier .joblib existe et qu'il se trouve à la racine{C.RESET}")
     
     def calculer_vecteurs_semantiques(self, texte_a_encoder=None) -> None:
         with torch.no_grad():
@@ -91,6 +99,13 @@ class NotesPredicteur:
         y_test_scaled = self._scaler.transform(y_test)
 
         self._mlp.fit(x_train, y_train_scaled.ravel())
+        
+        y_pred = self._mlp.predict(x_test)
+        mse = mean_squared_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+
+        print(f"{C.BLEU}Mean Squared Error: {mse:.4f}")
+        print(f"R² Score: {r2:.4f}{C.RESET}")
 
     def predire_score(self, texte_a_predire: str) -> float:
         # 1. On récupère le vecteur du texte unique
--- entrainement.py
+++ entrainement.py
@@ -1,24 +1,50 @@
 from composants.ia_notes import NotesPredicteur
+import os
 
 if __name__ == "__main__":
-    predicteur_notes = NotesPredicteur()
-    #predicteur_notes.charger_data("data/books_data_test.csv")
-    """
-    predicteur_notes.calculer_vecteurs_semantiques()
-    predicteur_notes.sauvegarder_embeddings() # Permet de sauvegarder le vecteur sémantique
-    predicteur_notes.lancer_entrainement()
-    predicteur_notes.sauvegarder_cerveau() # Permet de sauvegarder le cerveau (mlp et scaler)
-    """
-    
     """
     Pas besoin de charger le embedding si on charge le cerveau déjà entrainer.
     Mais si on charge un embedding pour entrainer l'ia de nouveau alors il faut
-    charger les données en utilisant .charger_data()
+    charger les données en utilisant .charger_data() avant.
     """
-    #predicteur_notes.charger_embeddings()  
-    #predicteur_notes.lancer_entrainement()
-    predicteur_notes.charger_cerveau()
+    predicteur_notes = NotesPredicteur()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    while True:
+        print("1 - charger_data")
+        print("2 - calculer_vecteurs_semantiques")
+        print("3 - sauvegarder embeddings")
+        print("4 - lancer entrainement")
+        print("5 - sauvegarder cerveau")
+        print("6 - charger cerveau")
+        print("7 - charger embeddings")
+        print("8 - predire note")
 
-    texte = "i love that book."
-    print(f"Score : {predicteur_notes.predire_score(texte)}")
+        choix = input("Choisissez une option :")
+        if choix == "1":
+            chemin = input("Entrez le chemin du csv (data/books_data.csv) :")
+            predicteur_notes.charger_data()
+        if choix == "2":
+            predicteur_notes.calculer_vecteurs_semantiques()
+        if choix == "3":
+            nom_fichier = input("Choisissez un nom de fichier (laisser vide pour le choix par défaut) :")
+            if nom_fichier != "":
+                predicteur_notes.sauvegarder_embeddings(nom_fichier)
+            predicteur_notes.sauvegarder_embeddings()
+        if choix == "4":
+            predicteur_notes.lancer_entrainement()
+        if choix == "5":
+            nom_fichier = input("Choisissez un nom de fichier (laisser vide pour le choix par défaut) :")
+            if nom_fichier != "":
+                predicteur_notes.sauvegarder_cerveau(nom_fichier)
+            predicteur_notes.sauvegarder_cerveau
+        if choix == "6":
+            predicteur_notes.charger_cerveau()
+        if choix == "7":
+            predicteur_notes.charger_embeddings()
+        if choix == "8":
+            texte = input("Entrez le texte dont vous voulez prédire le score :")
+            print(predicteur_notes.predire_score(texte))
+        else:
+            print("Entrez le nombre de la fonction à executer (1-8)")
+
     
